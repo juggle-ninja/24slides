c84f0c5c8a50c772ca79878488566469020d95b0
Sergey Grebeniuk <bc.serj@gmail.com>
2023-12-07 04:04:34 +0200

    tests

--- tests/Feature/API/V1/IssueControllerTest.php
+++ tests/Feature/API/V1/IssueControllerTest.php
@@ -3,13 +3,12 @@
 namespace Tests\Feature\API\V1;
 
 use App\Enums\IssueStatus;
-use App\Models\Issue;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use GuzzleHttp\Client;
 use Tests\TestCase;
 
 class IssueControllerTest extends TestCase
 {
+
     /**
      * A basic feature test example.
      */
@@ -22,47 +21,38 @@ class IssueControllerTest extends TestCase
 
     public function test_index_with_filters_with_story_points_as_string(): void
     {
-        $response = $this->json('get',  route('issues.index'),
-           [
+        $response = $this->json(
+            'get',
+            route('issues.index'),
+            [
                 'filters' => [
                     'is:status_id' => collect(IssueStatus::cases())->random()->value,
-                    'in:story_points' => collect(IssueStatus::cases())->random(rand(2, 5))->pluck('value')->implode(','),
+                    'in:story_points' => collect(IssueStatus::cases())->random(rand(2, 5))->pluck('value')->implode(
+                        ','
+                    ),
                     'contain:description' => fake()->word,
+                ]
             ]
-        ]);
+        );
 
         $response->assertStatus(200);
     }
 
     public function test_index_with_filters_with_story_points_as_array(): void
     {
-        $response = $this->json('get',  route('issues.index'),
+        $response = $this->json(
+            'get',
+            route('issues.index'),
             [
                 'filters' => [
                     '!is:status_id' => collect(IssueStatus::cases())->random()->value,
                     '!in:story_points' => collect(IssueStatus::cases())->random(rand(2, 4))->pluck('value')->toArray(),
                     '!contain:description' => fake()->word,
                 ]
-            ]);
-
-        $response->assertStatus(200);
-    }
-
-    public function test_index_with_filters_with_or_query_logic(): void
-    {
-        $response = $this->json(
-            'get',
-            route('issues.index'),
-            [
-                'filters' => [
-                    'status_id' => collect(IssueStatus::cases())->random()->value,
-                    'in:story_points' => collect(IssueStatus::cases())->random(rand(2, 4))->pluck('value')->toArray(),
-                    'contain:description' => fake()->word,
-                ],
-                'logic' => 'or'
             ]
         );
 
         $response->assertStatus(200);
     }
+
 }
